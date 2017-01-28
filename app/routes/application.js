6da6a8164a412da3cfe5cc5d641c6fa22aad06b5
mdshahrouq <mdshah1994@gmail.com>
2017-01-28 14:04:36 +0530

    Added cli-sass and bootstrap

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   setupController: function(controller) {
     controller.setProperties({ 
-      lat: 32.75494243654723,
-      lng: -86.8359375,
+      lat: 12.969388, 
+      lng: 77.579098,
       zoom: 4
     });
   }
